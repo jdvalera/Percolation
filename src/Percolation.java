6892becabfda7db138393d4210fd8c4406d9a1dd
jdvalera <johnlemuelvalera@yahoo.com>
2017-03-01 20:53:14 -0800

    Initial Percolation

---
+++ src/Percolation.java
@@ -0,0 +1,47 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+	
+	WeightedQuickUnionUF uf;
+	
+	
+	// create n-by-n grid, with all sites blocked
+	public Percolation(int n) {
+		
+	}
+	
+	// open site (row, col) if it is not open already
+	public void open(int row, int col) {
+		
+	}
+	
+	// is site (row, col) open?
+	public boolean isOpen(int row, int col) {
+		return false;
+		
+	}
+	
+	// is site (row, col) full?
+	public boolean isFull(int row, int col) {
+		return false;
+		
+	}
+	
+	// number of open sites
+	public int numberOfOpenSites() {
+		return 0;
+		
+	}
+	
+	// does the system percolate?
+	public boolean percolates() {
+		return false;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
