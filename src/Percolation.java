6b00c9c7103c49bfb1608b92e46c7579a96cc6c5
jdvalera <johnlemuelvalera@yahoo.com>
2017-03-01 21:15:24 -0800

    Added constructor

--- src/Percolation.java
+++ src/Percolation.java
@@ -4,12 +4,21 @@ public class Percolation {
 	
 	WeightedQuickUnionUF uf;
 	int gridSize;
+	// 0 - closed, 1 - open
 	byte[] openSites;
-	
+	int offset = 1;
+	byte closed = 0;
+	byte open = 1;
 	
 	// create n-by-n grid, with all sites blocked
 	public Percolation(int n) {
 		
+		uf = new WeightedQuickUnionUF(n+offset);
+		openSites = new byte[n+offset];
+		
+		for (int i=0; i<(n+offset); i++) {
+			openSites[i] = closed;
+		}
 	}
 	
 	// open site (row, col) if it is not open already
